286bd34462758bac467dba7d235a97d2328b36bd
sshekatk <sshekatk@indiana.edu>
2013-12-10 14:53:24 -0500

    Adding Log4j config. lines in VoiceBrowser.java class

--- src/com/browser/main/VoiceBrowser.java
+++ src/com/browser/main/VoiceBrowser.java
@@ -8,6 +8,9 @@ package com.browser.main;
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -16,20 +19,26 @@ import com.browser.controller.TabViewController;
 
 public class VoiceBrowser extends Application {
 
+	public static final Logger logger = Logger.getLogger(VoiceBrowser.class);
 	private Scene scene;
 	private TabViewController tabViewController;
 
 	public static void main(String[] args) {
+		PropertyConfigurator.configure("log4j.properties");
+		logger.info("Start of the application");
+
 		launch();
 	}
 
 	@Override
-	public void start(Stage stage) throws MalformedURLException, UnsupportedEncodingException {
+	public void start(Stage stage) throws MalformedURLException,
+			UnsupportedEncodingException {
 
 		stage.setTitle("Voice Based Browser");
 		tabViewController = new TabViewController();
-		
-		scene = new Scene(tabViewController.getBrowserTabBarView().getBrowserTabHolder(), 1020, 650);
+
+		scene = new Scene(tabViewController.getBrowserTabBarView()
+				.getBrowserTabHolder(), 1020, 650);
 		stage.setScene(scene);
 		stage.show();
 	}
